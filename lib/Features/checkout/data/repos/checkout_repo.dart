13f12b514972f63dcefc4efde38a38d0991240c9
Amir_Ahmed <amirmadian91@gmail.com>
2025-05-08 03:27:04 +0300

    create checkout repo and implentation

---
+++ lib/Features/checkout/data/repos/checkout_repo.dart
@@ -0,0 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:checkout_payment_ui/Features/checkout/data/models/payment_intent_input_model.dart';
+import 'package:checkout_payment_ui/core/errors/failures.dart';
+
+abstract class CheckoutRepo {
+  Future<Either<Failure, void>> makePayment({
+    required PaymentIntentInputModel paymentIntentInputModel,
+  });
+}
